b64dd888dd417c70b6d6565fa8c0ebc6a0e21bab
it23689794 <it23689794@my.sliit.lk>
2026-02-27 19:47:25 +0530

    Add project

--- js/data.js
+++ js/data.js
@@ -6,7 +6,7 @@ const portfolioData = {
         title: "Cloud Engineer & Problem Solver",
         bio: "I build exceptional digital experiences that make people's lives easier.",
         aboutDescription: "I am a Cloud Engineer and Frontend Developer with hands-on experience across AWS, Azure, and Google Cloud Platform (GCP). I specialize in designing, deploying, and managing scalable cloud infrastructure while building responsive and user-friendly frontend applications.I have practical experience with DevOps tools and practices, including Docker for containerization, CI/CD pipelines for automated builds and deployments, and Terraform for Infrastructure as Code (IaC). I enjoy automating workflows, optimizing cloud resources, and ensuring reliable, secure deployments from development to production.By combining cloud engineering, DevOps, and frontend development, I focus on delivering high-performance, scalable, and visually polished applications",
-        profileImageUrl: "https://github.com/Jayaru2003/images/blob/main/DF%20(421).jpg?raw=true",
+        profileImageUrl: "https://raw.githubusercontent.com/Jayaru2003/Portfolio/refs/heads/images/DF%20(421).jpg",
         cvUrl: "#", // Link to your CV/Resume
         emailAddress: "jayarumanilka@gmail.com",
         phoneNumber: "+94 70 679 6634",
@@ -25,17 +25,17 @@ const portfolioData = {
             id: 1,
             title: "Teslang Compiler",
             description: "A Create a DSL (TestLang++) to describe HTTP tests (GET/POST/PUT/DELETE, headers, body, assertions). Implement a scanner and parser (Lex/Yacc or JFlex/CUP) that translate .test files into a single GeneratedTests.java JUnit 5 class. Compile and run the generated tests against a local backend",
-            imageUrl: "https://raw.githubusercontent.com/Jayaru2003/images/refs/heads/main/WhatsApp%20Image%202026-01-09%20at%205.53.51%20PM.jpeg",
+            imageUrl: "https://raw.githubusercontent.com/Jayaru2003/Portfolio/refs/heads/images/testlang1.png",
             techStack: "Lex,Yacc,Java Spring Boot",
             githubUrl: "https://github.com/Jayaru2003/testlang-compiler",
-           // demoUrl: "https://demo-project1.com"
+            demoUrl: "https://drive.google.com/file/d/1eQanehz7EitVN0xVswIWowzDMdkmodwf/view?usp=sharing"
         },
      
      {
             id: 2,
             title: "Cloth-Heavan E-commerce Website",
             description: "Cloth Haven is a modern full-stack e-commerce application built using Spring Boot for the backend and React + TypeScript for the frontend. The platform includes secure user authentication, profile management, a responsive UI, and a fully structured REST API connected to a MySQL database. Core e-commerce features such as product management, shopping cart, order processing, and payments are currently in development.",
-            imageUrl: "https://raw.githubusercontent.com/Jayaru2003/images/refs/heads/main/WhatsApp%20Image%202026-01-09%20at%205.53.51%20PM.jpeg",
+            imageUrl: "https://raw.githubusercontent.com/Jayaru2003/Portfolio/refs/heads/images/Cloth-heaven.png",
             techStack: "Java,spring boot,HTML,CSS,JavaScript,react",
             githubUrl: "https://github.com/Jayaru2003/Cloth-Haven.git",
             demoUrl: "https://drive.google.com/file/d/1ciE72vGYggRwao9NxhAWwWi5lW4zZrIM/view?usp=drivesdk"
